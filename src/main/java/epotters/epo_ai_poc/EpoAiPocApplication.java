55f7dd21153cb0eae3d6071695dfcbcc77f7a9b6
Eelko Potters <epotters@xs4all.nl>
2026-04-10 11:54:05 +0200

    Generated application setup using Spring Initializr

---
+++ src/main/java/epotters/epo_ai_poc/EpoAiPocApplication.java
@@ -0,0 +1,13 @@
+package epotters.epo_ai_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpoAiPocApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(EpoAiPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epotters/epo_ai_poc/EpoAiPocApplicationTests.java
@@ -0,0 +1,13 @@
+package epotters.epo_ai_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpoAiPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
